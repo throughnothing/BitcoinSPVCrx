371b083cceab7d4b619b0af9d70babadf6e0efce
William Wolf <throughnothing@gmail.com>
2015-02-17 09:07:05 -0800

    Move qrcode.js to js/vendor/

--- js/app/index.js
+++ js/app/index.js
@@ -2,8 +2,8 @@
 
 require ('bootstrap-webpack!../../bootstrap.config.js');
 var $ = require('jquery'),
-    QRCode = require('./qrcode'),
     React = require('react'),
+    QRCode = require('../vendor/qrcode'),
     router = require('./router'),
     messaging = require('../chrome/messaging');
 
